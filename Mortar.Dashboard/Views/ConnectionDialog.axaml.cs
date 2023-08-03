036d58de436a21365136e31b4f729dca60f748ab
Aleks Rutins <aleks@rutins.com>
2023-08-02 20:14:42 -0400

    Use Avalonia for UI

---
+++ Mortar.Dashboard/Views/ConnectionDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mortar.Dashboard.Views;
+
+public partial class ConnectionDialog : Window
+{
+    public ConnectionDialog()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Mortar.Dashboard/Views/LoadingScreen.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Mortar.Dashboard.Views;
+
+public class LoadingScreen : TemplatedControl
+{
+}
